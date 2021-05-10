0b32d3e2bebcd377ed4be2cf0cc3b2cb4d2ca7c6
Shogo Akiyama <sakiyama@knox.edu>
2021-05-10 02:49:41 -0700

    Start TS code to get form from template and render in the app div

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    console.log('hi')
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+}
+
+const projectInput = new ProjectInput();
