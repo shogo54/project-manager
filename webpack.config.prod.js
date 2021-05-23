298079ca560cab4c5658a360d87ba80c53d37ed8
Shogo Akiyama <sakiyama@knox.edu>
2021-05-22 23:47:56 -0700

    Format the webpack.config files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,14 +13,12 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin(),
-  ]
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
